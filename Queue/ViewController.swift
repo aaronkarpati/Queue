609b7e7a9450bf539e0f6c7d29f614c7ba6a3635
Karpati Aaron <aronkarpati@aron-macbook-pro.local>
2018-11-10 19:01:12 +0100

    Initial Commit

---
+++ Queue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queue
+//
+//  Created by Áron Kárpáti on 2018. 11. 10..
+//  Copyright © 2018. Áron Kárpáti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
